d08044f46eaa7f8c73cfada42885f13c95f1b644
Badal56 <61780175+Badal56@users.noreply.github.com>
2021-06-22 00:44:11 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,37 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+
+app = Flask(__name__)
+model = pickle.load(open('final_diabetics.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('index1.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    int_features = [float(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+
+    output =prediction
+
+    return render_template('index1.html', prediction_text=' Diabetics is $ {}'.format(output))
+
+@app.route('/predict_api',methods=['POST'])
+def predict_api():
+    '''
+    For direct API calls trought request
+    '''
+    data = request.get_json(force=True)
+    prediction = model.predict([np.array(list(data.values()))])
+
+    output = prediction[0]
+    return jsonify(output)
+
+if __name__ == "__main__":
+    app.run(debug=True)
